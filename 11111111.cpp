0e1a700f6480c0a8597603e04e5faa0dd4a0ed25
zsyyb <77322691+zsyyb@users.noreply.github.com>
2021-02-24 10:38:23 +0800

    上传c++文件
    这是我第一次上传文件

---
+++ 11111111.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a(0),b(0),c;
+	cin>>c;
+	if(c>a*b)
+	{
+		cout<<a;	
+	}
+	else
+	{
+		cout<<b;	
+	}
+	return 0;
+}
---
+++ Cpp13.2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	double x,y;
+	cin>>x;
+	if(x<=-1)
+		y=3.0*sqrt(fabs(x)+2.0)+1;
+	else if(x<=10)
+		y=pow(x,5)+1;
+	else
+		y=sqrt((x+1)/(2*pow(x,2)));
+	cout<<y<<endl;
+	return 0;
+}
+
